56609d2b792241622c03bcb1922b89f5a90f172f
GustavoRaguso <34249528@comunidadort.edu.ar>
2025-10-30 13:18:03 -0300

    Creacion de proyecto MVC

---
+++ Carrito/Carrito/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Carrito.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
