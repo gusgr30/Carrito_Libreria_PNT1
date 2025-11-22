c52a6baed7b2076542b4dde289cfbddeeaa33912
ORTLucas <42955954@comunidadort.edu.ar>
2025-11-22 19:35:52 -0300

    Buscar

---
+++ Carrito/Carrito/Controllers/LibroController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Carrito.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Carrito.Controllers
+{
+    public class LibroController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public LibroController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var lista = _context.Libros.ToList();
+            return View(lista);
+        }
+
+        public IActionResult Buscar(string titulo)
+        {
+            if (string.IsNullOrWhiteSpace(titulo))
+            {
+                return View("Index", _context.Libros.ToList());
+            }
+
+            var filtrados = _context.Libros
+                .Where(l => l.Title.Contains(titulo))
+                .ToList();
+
+            return View("Index", filtrados);
+        }
+    }
+}
